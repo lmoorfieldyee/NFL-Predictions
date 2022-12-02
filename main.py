0e2927f7dca95ef0c8cd54df242d39fc116b1f69
Liam Moorfield-Yee <lmoorfieldyee@gmail.com>
2022-12-02 11:56:00 -0500

    initial commit of nfl prediction program.

---
+++ main.py
@@ -0,0 +1,191 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+import time
+from sklearn.model_selection import cross_validate
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from numpy import *
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from utils import *
+
+
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+week = int(input("Enter current week's matchup: "))
+week_folder = r"C:\Users\lmoor\Desktop\Data Science Projects\Football Outcome Prediction\week " + str(week)
+if len(os.listdir(week_folder)) == 0:
+    get_clean_data(2022, week)
+    get_clean_data(2021, week)
+    get_clean_data(2020, week)
+    get_clean_data(2019, week)
+    get_clean_data(2018, week)
+
+df_2022 = pd.read_csv(r"C:\Users\lmoor\Desktop\Data Science Projects\Football Outcome Prediction\week " + str(week) + "\df_2022.csv")
+df_2021 = pd.read_csv(r"C:\Users\lmoor\Desktop\Data Science Projects\Football Outcome Prediction\week " + str(week) + "\df_2021.csv")
+df_2020 = pd.read_csv(r"C:\Users\lmoor\Desktop\Data Science Projects\Football Outcome Prediction\week " + str(week) + "\df_2020.csv")
+df_2019 = pd.read_csv(r"C:\Users\lmoor\Desktop\Data Science Projects\Football Outcome Prediction\week " + str(week) + "\df_2019.csv")
+df_2018 = pd.read_csv(r"C:\Users\lmoor\Desktop\Data Science Projects\Football Outcome Prediction\week " + str(week) + "\df_2018.csv")
+
+all_df = pd.concat([df_2022, df_2021, df_2020, df_2019, df_2018])
+
+train_df = all_df[(all_df['season']!=2022) & (all_df['week']>=7)]
+predict_df = all_df[(all_df['season']==2022) & (all_df['week']==week)]
+
+'''
+testing out which features to include in the model. many of the variables are co-related and therefore we should not 
+include all of them. I.e. 'total yards scored' is the sum of 'total pass yards' and 'total rush yards' so we should 
+use one or the other. For this model feature set 6 produces the best results.
+
+feature sets roc_auc_scores are below:
+features0 = 0.6677205300632911
+features1 = 0.6748164556962026
+features2 = 0.6838587816455696
+features3 = 0.6865810917721518 (alpha=1)
+features4 = 0.6883500791139241 (alpha=1)
+features5 = 0.6863334651898734 (alpha=1)
+features6 = 0.6884422468354431 (alpha=1)
+features7 = 0.6841538765822784 (alpha=.1)
+features8 = 0.6883995253164557 (alpha =.1)
+features9 = 0.6856863132911393 (alpha = 1)
+features10 = 0.6852167721518987 (alpha = 1)
+features_all = 0.6863334651898734
+'''
+
+'''
+X_features0 = ['win ratio', 'opp win ratio']
+
+X_features1 = ['win ratio', 'opp win ratio', 'home or away']
+
+X_features2 = ['win ratio', 'opp win ratio', 'home or away', 'total points scored szn avg', 'opp total points scored szn avg']
+
+X_features3 = ['win ratio', 'opp win ratio', 'home or away', 'total points scored szn avg', 'opp total points scored szn avg',
+              'total points allowed szn avg', 'opp total points allowed szn avg']
+
+X_features4 = ['win ratio', 'opp win ratio', 'home or away', 'total points scored szn avg', 'opp total points scored szn avg',
+              'total points allowed szn avg', 'opp total points allowed szn avg', 'total yards against szn avg', 
+              'opp total yards against szn avg']
+
+X_features5 = ['win ratio', 'opp win ratio', 'home or away', 'total points scored szn avg', 'opp total points scored szn avg',
+              'total points allowed szn avg', 'opp total points allowed szn avg', 'total yards against szn avg', 
+              'opp total yards against szn avg', 'total yards gained szn avg', 'opp total yards gained szn avg']
+
+
+
+X_features6 = ['win ratio', 'opp win ratio', 'home or away', 'total points scored szn avg', 'opp total points scored szn avg',
+               'total yards against szn avg', 'opp total yards against szn avg']
+
+X_features7 = ['win ratio', 'opp win ratio', 'home or away', 'total points scored szn avg', 'opp total points scored szn avg',
+               'total yards against szn avg', 'opp total yards against szn avg', 'total yards gained szn avg', 
+              'opp total yards gained szn avg']
+
+X_features8 = ['win ratio', 'opp win ratio', 'home or away', 'total points scored szn avg', 'opp total points scored szn avg',
+               'total yards against szn avg', 'opp total yards against szn avg', 'first downs gained szn avg',
+              'opp first downs gained szn avg']
+
+X_features9 = ['win ratio', 'opp win ratio', 'home or away', 'total points scored szn avg', 'opp total points scored szn avg',
+               'total yards against szn avg', 'opp total yards against szn avg', 'turnovers gained by defense szn avg',
+               'opp turnovers gained by defense szn avg']
+
+X_features10 = ['win ratio', 'opp win ratio', 'home or away', 'total points scored szn avg', 'opp total points scored szn avg',
+               'total yards against szn avg', 'opp total yards against szn avg', 'turnovers lost szn avg',
+               'opp turnovers lost szn avg']
+
+X_features_all = ['home or away', 'win ratio',
+                  'total points scored szn avg', 'total points allowed szn avg',
+                  'first downs gained szn avg',
+                  'total yards gained szn avg', 'pass yards gained szn avg',
+                  'rush yards gained szn avg', 'turnovers lost szn avg',
+                  'first downs allowed szn avg', 'total yards against szn avg',
+                  'pass yards against szn avg', 'rush yards against szn avg',
+                  'turnovers gained by defense szn avg',
+                  'opp total points scored szn avg', 'opp total points allowed szn avg',
+                  'opp first downs gained szn avg', 'opp total yards gained szn avg',
+                  'opp pass yards gained szn avg', 'opp rush yards gained szn avg',
+                  'opp turnovers lost szn avg', 'opp first downs allowed szn avg',
+                  'opp total yards against szn avg', 'opp pass yards against szn avg',
+                  'opp rush yards against szn avg',
+                  'opp turnovers gained by defense szn avg', 'opp win ratio']
+'''
+
+
+X_features6 = ['win ratio', 'opp win ratio', 'home or away', 'total points scored szn avg', 'opp total points scored szn avg',
+               'total yards against szn avg', 'opp total yards against szn avg']
+
+
+X = train_df[X_features6]
+y = train_df['win or loss']
+X_predict = predict_df[X_features6]
+
+#normalizing values
+scaler = MinMaxScaler()
+X_train_scaled = scaler.fit_transform(X)
+X_predict_scaled = scaler.transform(X_predict)
+
+alphas = [.001, .01, .1, 1, 10, 100]
+
+for alpha in alphas:
+    print("current alpha {}".format(alpha))
+    clf = LogisticRegression(max_iter=10000, penalty='l2', C=alpha)
+    # clf.fit(X_train_scaled, y_train)
+    results = cross_validate(estimator=clf,
+                             X=X_train_scaled,
+                             y=y,
+                             cv=5,
+                             scoring=['accuracy', 'roc_auc'],
+                             return_train_score=True)
+
+    print('training accuracy of: {}'.format(results['train_accuracy'].mean()))
+    print('test accuracy of: {}'.format(results['test_accuracy'].mean()))
+    print('training roc: {}'.format(results['train_roc_auc'].mean()))
+    print('test roc: {}'.format(results['test_roc_auc'].mean()))
+    print('\n')
+
+
+X = train_df[X_features6]
+y = train_df['win or loss']
+X_predict = predict_df[X_features6]
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+#normalizing values
+scaler = MinMaxScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+X_predict_scaled = scaler.transform(X_predict)
+
+#print(week_to_predict.head(2)[['team_name', 'opponent name', 'total points scored szn avg', 'total points allowed szn avg',
+#                               'opp total points scored szn avg', 'opp total points allowed szn avg']])
+clf = LogisticRegression(max_iter=10000, penalty='l2', C=1)
+clf.fit(X_train_scaled, y_train)
+
+
+print("train accuracy score: ", accuracy_score(y_train, clf.predict(X_train_scaled)))
+print("test accuracy score: ", accuracy_score(y_test, clf.predict(X_test_scaled)))
+print("train roc score: ", roc_auc_score(y_train, clf.predict_proba(X_train_scaled)[:, 1]))
+print("test roc score: ", roc_auc_score(y_test, clf.predict_proba(X_test_scaled)[:, 1]))
+
+
+
+predictions = clf.predict(X_predict_scaled)
+predictions_proba = clf.predict_proba(X_predict_scaled)
+
+week_predictions = pd.DataFrame({'team': list(predict_df['team_name']), 'opponent':list(predict_df['opponent name']),
+                                    'win': list(predictions),
+                                    'win probability': list(predictions_proba)})
+week_predictions['win probability'] = week_predictions['win probability'].apply(lambda x: x[1])
+week_predictions['payoff needed'] = 1 / week_predictions['win probability']
+save_path = week_folder + '\\week_predictions.csv'
+#week_predictions.to_csv(save_path)
+print(week_predictions)
---
+++ utils.py
@@ -0,0 +1,233 @@
+import numpy as np
+import pandas as pd
+import time
+from numpy import *
+
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+def get_pfr_data(year):
+    team_name_dict = {"buffalo bills": 'buf',
+             "new york jets": "nyj",
+             "miami dolphins": 'mia',
+             "new england patriots": "nwe",
+             "baltimore ravens": 'rav',
+             "cincinnati bengals": 'cin',
+             "cleveland browns": 'cle',
+             'pittsburgh steelers': 'pit',
+             'tennessee titans': 'oti',
+             'indianapolis colts': 'clt',
+             "jacksonville jaguars": 'jax',
+             'houston texans': 'htx',
+             'kansas city chiefs': 'kan',
+             'los angeles chargers': 'sdg',
+             'denver broncos': 'den',
+             'las vegas raiders': 'rai',
+             'philadelphia eagles': 'phi',
+             'dallas cowboys': 'dal',
+             'new york giants': 'nyg',
+             'washington commanders': 'was',
+             'minnesota vikings': 'min',
+             'greenbay packers': 'gnb',
+             'chicago bears': 'chi',
+             'detroit lions': 'det',
+             'atlanta falcons': 'atl',
+             'tampa bay buccaneers': 'tam',
+             'new orleans saints': 'nor',
+             'carolina panthers': 'car',
+             'seattle seahawks': 'sea',
+             'san francisco 49ers': 'sfo',
+             'los angeles rams': 'ram',
+             'arizona cardinals': 'crd'
+             }
+    # create url function
+    def make_url(team_name, year):
+        url = 'https://www.pro-football-reference.com/teams/' + str(team_name) + '/' + str(year) + '.htm'
+        return url
+
+    team_names = list(team_name_dict.values())
+    pfr_data = []
+    # for loop to iterate through all teams and extract data
+    for key, team_name in team_name_dict.items():
+        # print(team_name)
+
+        # creating url to extract data from
+        url = make_url(team_name, year)
+        print(url)
+        # extract out team season table, which is the second to last table on the page.
+        team_data = pd.read_html(url)[-2]
+
+        # dataframe comes with a multilevel index. First level is nothing, so we're dropping it
+        team_data = team_data.droplevel(0, axis=1)
+
+        # creating team name column
+        team_data['team_name'] = key
+
+        # adding season year column
+        team_data['season'] = year
+
+
+        pfr_data.append(team_data)
+
+        # Telling the function to go to sleep for 2 seconds before sending another request to website
+        # Too many requests too quickly results in a 'HTTP error 429 (Too Many Requests)''
+        time.sleep(2)
+    return pfr_data
+
+def clean_pfr(df, week_to_predict):
+    team_data = df
+    team_data.drop(['Date', 'Day', 'OT', 'Unnamed: 3_level_1', 'Unnamed: 4_level_1', 'Offense', 'Defense',
+                    'Sp. Tms'], inplace=True, axis=1)
+    # renaming remaining columns into more readable names.
+    col_renaming = ['week', 'win or loss', 'season record', 'home or away', 'opponent name',
+                    'total points scored', 'total points allowed', 'first downs gained',
+                    'total yards gained', 'pass yards gained', 'rush yards gained', 'turnovers lost',
+                    'first downs allowed', 'total yards against', 'pass yards against',
+                    'rush yards against', 'turnovers gained by defense', 'team_name', 'season']
+    team_data.columns = col_renaming
+
+    # making opponent names lowercase
+    team_data['opponent name'] = team_data['opponent name'].str.lower()
+
+    # Only keeping data up until the week of interest.
+    team_data = team_data.iloc[0:week_to_predict]
+
+    # extract out prediction week so it doesn't get dropped when we remove bye weeks as there will be null values for
+    # latest season.
+    prediction_data = pd.DataFrame(team_data[team_data['week'] == week_to_predict])
+
+    team_data = team_data[team_data['week'] != week_to_predict]
+
+    # removing bye weeks which have no data
+    team_data = team_data[~team_data['win or loss'].isnull()]
+
+    # Adding prediction week back to dataframe
+    team_data = pd.concat([team_data, prediction_data])
+
+    # convert home or away column to a dummy variable. 0 for home, 1 for away.
+    team_data['home or away'].replace("\@", "1", inplace=True, regex=True)
+    team_data['home or away'].replace(np.nan, "0", inplace=True)
+    team_data['home or away'].replace("[a-zA-Z]", "1", regex=True, inplace=True)
+    team_data['home or away'] = team_data['home or away'].astype('float64')
+
+    # removing all ties as these happen so infrequently since the introduction of OT
+    team_data = team_data[team_data['win or loss'] != 'T']
+
+    # convert win or loss into numeric. 0 for loss, 1 for a win
+    team_data['win or loss'].replace("W", 1, regex=True, inplace=True)
+    team_data['win or loss'].replace("L", 0, regex=True, inplace=True)
+
+    # converting target values into a float
+    team_data['win or loss'] = team_data['win or loss'].astype('float64')
+
+    # Any previous season's data (i.e. not 2022) may have some string values in the week column (i.e. 'playoff')
+    # so we need to make sure that the week column is not still an obj and convert it to a float value.
+    team_data['week'] = team_data['week'].astype('float64')
+
+    team_data = team_data[team_data['opponent name'] != 'bye week']
+
+
+    # these two columns have nan's where they should have 0's
+    team_data['turnovers lost'].replace(np.nan, 0, inplace=True)
+    team_data['turnovers gained by defense'].replace(np.nan, 0, inplace=True)
+
+    #updating two team names to match our dictionary
+    teams_to_rename = {"green bay packers": "greenbay packers", "washington football team": "washington commanders",
+                       "washington redskins": "washington commanders", "oakland raiders": "las vegas raiders"}
+    team_data.replace(teams_to_rename, regex=True, inplace=True)
+
+    # only keep up until week of interest
+    team_data = team_data[team_data['week'] <= week_to_predict]
+
+    return team_data
+
+
+
+def create_feature_cols(team_data_list):
+
+    def get_season_avg(df, column):
+        weeks_played = len(df)
+        avg_szn = [np.nan]
+        for i in range(weeks_played):
+            if i == 0:
+                continue
+            else:
+                weekly_szn_avg = df[0:i][column].mean()
+                avg_szn.append(weekly_szn_avg)
+
+        return avg_szn
+
+
+    for _, team_data in enumerate(team_data_list):
+        #creating win ratio feature by splitting the column "season record" into individual
+        # win and loss columns
+        team_data['wins'] = team_data['season record'].str.split('-').str[0].astype('float64')
+        team_data['losses'] = team_data['season record'].str.split('-').str[1].astype('float64')
+        team_data['season record'] = team_data['season record'].astype('str')
+        team_data['OT losses'] = team_data['season record'].apply(lambda cell:
+                                                                  cell.split('-')[-1] if len(cell.split('-'))>2
+                                                                  else 0).astype('float64')
+        team_data['win ratio'] = team_data['wins'] / (team_data['wins'] + team_data['losses'] + team_data['OT losses'])
+
+        #update win/loss ratio to represent win losses heading into current game.
+        #the data includes the win or loss data from current week which we will not know ahead of time.
+        record = [np.nan]
+        for wr in team_data['win ratio']:
+            record.append(wr)
+        team_data['win ratio'] = record[0:-1]
+
+        #creating season long averages to be used as features
+        #
+        cols_to_create = ['total points scored', 'total points allowed', 'first downs gained',
+                                  'total yards gained', 'pass yards gained', 'rush yards gained',
+                                  'turnovers lost', 'first downs allowed', 'total yards against',
+                                  'pass yards against', 'rush yards against',
+                                  'turnovers gained by defense']
+        for col in cols_to_create:
+            new_col = col + " szn avg"
+            team_data[new_col] = get_season_avg(team_data, col)
+
+    return team_data_list
+
+
+def get_opponent_stats(df, col):
+    col_to_return = []
+    opponent_name = list(df['opponent name'])
+    week = list(df['week'])
+
+    print(df[(df['team_name']=='buffalo bills') & (df['week']==2)]['total points scored szn avg'].values[0])
+    #print(df[(df['opponent name']==opponent_name[0]) & (df['week']==week[0])][col])
+    for i in range(len(opponent_name)):
+        print(opponent_name[i])
+        print(week[i])
+        print(df[(df['team_name']==opponent_name[i]) & (df['week']==week[i])])
+        # print('opponent name: ', opponent_name[i], 'week: ', week[i])
+        col_value = df[(df['team_name']==opponent_name[i]) & (df['week']==week[i])][col].values[0]
+        col_to_return.append(col_value)
+    return col_to_return
+
+def get_clean_data(year, week):
+    week_folder = r"C:\Users\lmoor\Desktop\Data Science Projects\Football Outcome Prediction\week " + str(week) + '\df_' + str(year) + '.csv'
+    x = get_pfr_data(year)
+    for i, df in enumerate(x):
+        x[i] = clean_pfr(df, week)
+    x = create_feature_cols(x)
+    x = pd.concat(x)
+    cols_to_add = ['win ratio', 'total points scored szn avg',
+                   'total points allowed szn avg', 'first downs gained szn avg',
+                   'total yards gained szn avg', 'pass yards gained szn avg',
+                   'rush yards gained szn avg', 'turnovers lost szn avg',
+                   'first downs allowed szn avg', 'total yards against szn avg',
+                   'pass yards against szn avg', 'rush yards against szn avg',
+                   'turnovers gained by defense szn avg']
+
+    for col in cols_to_add:
+        new_col = "opp " + col
+        x[new_col] = get_opponent_stats(x, col)
+
+    x.to_csv(week_folder)
+
+
+
+
